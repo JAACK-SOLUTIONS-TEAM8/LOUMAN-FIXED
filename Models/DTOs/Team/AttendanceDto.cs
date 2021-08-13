3d5d983c6ae087fee91b46faec4f35ed3204656b
kayyborra <u19020393@tuks.co.za>
2021-08-13 18:09:55 +0200

    Added Team DTOs

---
+++ Models/DTOs/Team/AttendanceDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Models.DTOs.Team
+{
+    public class AttendanceDto
+    {
+        public int AttendanceId { get; set; }
+        public int AttendanceHistoryId { get; set; }
+        public int TeamId { get; set; }
+        public DateTime Date { get; set; }
+        public int EmployeeId { get; set; }
+        public string Initials { get; set; }
+        public string Surname { get; set; }
+        public bool Present { get; set; }
+        public bool Absent { get; set; }
+        public string Reason { get; set; }
+    }
+}
