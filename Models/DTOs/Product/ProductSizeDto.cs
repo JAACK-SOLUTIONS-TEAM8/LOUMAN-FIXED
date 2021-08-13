3de68e8743268cff450deb589ec9637c4aa04da6
JayyyHarden <u19107430@tuks.co.za>
2021-08-13 18:14:36 +0200

    Added Product DTOs

---
+++ Models/DTOs/Product/ProductSizeDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Models.DTOs.Product
+{
+    public class ProductSizeDto
+    {
+        public int ProductSizeId { get; set; }
+        public string ProductSizeDescription { get; set; }
+    }
+}
---
+++ Models/DTOs/Product/StockDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Models.DTOs.Product
+{
+    public class StockDto
+    {
+        public int StockId { get; set; }
+        public int ProductId { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
