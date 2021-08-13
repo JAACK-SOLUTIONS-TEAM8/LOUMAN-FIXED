64f8253f472e94b83cc8474ac7c0ee5abc3171a0
JayyyHarden <u19107430@tuks.co.za>
2021-08-13 17:50:27 +0200

    Added User Entities

---
+++ Models/Entities/UserEntity.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Models.Entities
+{
+    [Table("User")]
+    public class UserEntity
+    {
+        [Key]
+        public int UserId { get; set; }
+        public int UserTypeId { get; set; }
+        public int? AddressId { get; set; }
+
+        public string IdNumber { get; set; }
+
+        public string Initials { get; set; }
+        public string Surname{ get; set; }
+        public string CellNumber{ get; set; }
+        public string Email{ get; set; }
+        public string UserName{ get; set; }
+        public string Password{ get; set; }
+        public bool isDeleted{ get; set; }
+    }
+}
---
+++ Models/Entities/UserTypeEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Models.Entities
+{
+    [Table("UserType")]
+    public class UserTypeEntity
+    {
+        [Key]
+
+        public int UserTypeId { get; set; }
+        public string UserTypeDescription { get; set; }
+        public bool isDeleted { get; set; }
+    }
+}
