aeae6ba2fca563aaa82887b16f3f25e5ff975499
Ammaar1803 <u18080945@tuks.co.za>
2021-08-14 14:03:03 +0200

    Changed the Auth Controller

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Louman.Models.DTOs.User;
+using Louman.Repositories.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,11 +9,58 @@ using System.Threading.Tasks;
 
 namespace Louman.Controllers
 {
-    public class AuthController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly IAuthRepository _authRepository;
+
+        public AuthController(IAuthRepository authRepository)
         {
-            return Ok();
+            _authRepository = authRepository;
         }
+
+        [HttpPost("Login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto user)
+        {
+            var existingUser = await _authRepository.LoginAsync(user);
+            if (existingUser != null)
+                return Ok(new { User = existingUser, StatusCode = StatusCodes.Status200OK });
+            return Ok(new { User = existingUser, StatusCode = StatusCodes.Status404NotFound });
+        }
+
+
+        [HttpGet("Logout/{id}")]
+        public async Task<IActionResult> Logout([FromRoute] int id)
+        {
+            var user = await _authRepository.LogoutAsync(id);
+            if (user != false)
+                return Ok(new { User = user, StatusCode = StatusCodes.Status200OK });
+            return Ok(new { User = user, StatusCode = StatusCodes.Status404NotFound });
+        }
+
+        [HttpPost("ResetPassword")]
+        public async Task<IActionResult> ResetPassword([FromBody] UserInfoDto user)
+        {
+            var response = await _authRepository.ResetPassword(user);
+            if (response == true)
+                return Ok(new { Response = true, StatusCode = StatusCodes.Status200OK });
+            return Ok(new { Response = false, StatusCode = StatusCodes.Status404NotFound });
+
+        }
+
+
+
+
+        [HttpGet("Forget")]
+        public async Task<IActionResult> ForgetPassword([FromRoute] string email)
+        {
+            var response = await _authRepository.isEmailExist(email);
+            if (response == true)
+                return Ok(new { Response = true, StatusCode = StatusCodes.Status200OK });
+            return Ok(new { Response = false, StatusCode = StatusCodes.Status404NotFound });
+
+        }
+
     }
 }
