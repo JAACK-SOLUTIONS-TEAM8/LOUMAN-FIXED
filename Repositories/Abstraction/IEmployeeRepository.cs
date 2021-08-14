0a5cc8857960e456ff4a2415460fe3c1a9f8935a
kayyborra <u19020393@tuks.co.za>
2021-08-14 22:22:36 +0200

    added abstractions(add &delete for emp)

---
+++ Repositories/Abstraction/IEmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using Louman.Models.DTOs.Employee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Repositories.Abstraction
+{
+    public interface IEmployeeRepository
+    {
+        Task<EmployeeDto> Add(EmployeeDto employee);
+        Task<bool> DeleteAsync(int employeeUserId);
+        
+
+
+    }
+}
