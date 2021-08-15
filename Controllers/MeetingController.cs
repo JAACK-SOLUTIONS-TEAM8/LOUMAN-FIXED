3fefc262d0def50275f8711317cd463a4a506b10
Caitlinbraga <62016034+Caitlinbraga@users.noreply.github.com>
2021-08-15 19:53:11 +0200

    added meeting controller

---
+++ Controllers/MeetingController.cs
@@ -0,0 +1,37 @@
+﻿using Louman.Models.DTOs;
+using Louman.Models.DTOs.Meeting;
+using Louman.Repositories.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeetingController : ControllerBase
+    {
+        private readonly IMeetingRepository _meetingRepository;
+
+        public MeetingController(IMeetingRepository meetingRepository)
+        {
+            _meetingRepository = meetingRepository;
+        }
+
+        [HttpGet("Slot/All")]
+        public async Task<IActionResult> GetAllSlots()
+        {
+            var slots = await _meetingRepository.GetAllSlots();
+            if (slots != null)
+                return Ok(new { Slots=slots,StatusCode=StatusCodes.Status200OK});
+            return NotFound(new { Slots=slots,StatusCode=StatusCodes.Status404NotFound});
+
+        }
+
+       
+
+    }
+}
