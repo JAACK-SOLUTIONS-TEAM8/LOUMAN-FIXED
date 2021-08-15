5f1d2a10e5d581ea629a35f850e597c4a72b2549
Caitlinbraga <62016034+Caitlinbraga@users.noreply.github.com>
2021-08-15 13:44:46 +0200

    Added get all meeting method

---
+++ Repositories/Behavior/MeetingRepository.cs
@@ -0,0 +1,46 @@
+﻿using Louman.AppDbContexts;
+using Louman.Models.DTOs;
+using Louman.Models.DTOs.Meeting;
+using Louman.Models.Entities;
+using Louman.Repositories.Abstraction;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Repositories.Behavior
+{
+    public class MeetingRepository : IMeetingRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public MeetingRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+       
+
+        public async Task<List<SlotDto>> GetAllSlots()
+        {
+            return await (from s in _dbContext.Slots
+                         join u in _dbContext.Users on s.AdminUserId equals u.UserId
+                         where s.isDeleted == false && s.isBooked==false
+                         select new SlotDto
+                         {
+                             Date=s.Date,
+                            SlotId=s.SlotId,
+                            isBooked=s.isBooked,
+                            AdminUserId=u.UserId,
+                            StartTime=s.StartTime.ToString("F"),
+                            EndTime=s.EndTime.ToString("F")
+
+                         }).ToListAsync();
+        }
+
+ 
+
+    }
+}
