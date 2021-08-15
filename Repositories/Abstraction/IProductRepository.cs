c6b5ba4312b0ad0e988371fd7d445d97437773d6
JayyyHarden <u19107430@tuks.co.za>
2021-08-15 15:32:43 +0200

    Added Add Product Abstraction

---
+++ Repositories/Abstraction/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using Louman.Models.DTOs.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Repositories.Abstraction
+{
+    public interface IProductRepository
+    {
+        
+
+        Task<ProductDto> AddProduct(ProductDto product);
+       
+
+    }
+}
