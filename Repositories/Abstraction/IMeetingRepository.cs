040d2053ddd00444bca57a5a63829151b6b49994
Caitlinbraga <62016034+Caitlinbraga@users.noreply.github.com>
2021-08-15 13:47:04 +0200

    added get all meeting abstraction

---
+++ Repositories/Abstraction/IMeetingRepository.cs
@@ -0,0 +1,16 @@
+﻿using Louman.Models.DTOs;
+using Louman.Models.DTOs.Meeting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Repositories.Abstraction
+{
+    public interface IMeetingRepository
+    {
+        
+        Task<List<SlotDto>> GetAllSlots();
+       
+    }
+}
