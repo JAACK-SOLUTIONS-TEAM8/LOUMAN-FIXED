5a795eff534c658a162b4084a57a88c1514b21b8
Ammaar1803 <u18080945@tuks.co.za>
2021-08-16 15:40:05 +0200

    Get all client behavior

--- Repositories/Behavior/ClientRepository.cs
+++ Repositories/Behavior/ClientRepository.cs
@@ -126,7 +126,34 @@ namespace Louman.Repositories
             return null;
 
         }
+        public async Task<List<ClientDto>> GetAllAsync()
+        {
+            return await (from u in _dbContext.Users
+                          join c in _dbContext.Clients on u.UserId equals c.UserId
+                          join a in _dbContext.Addresses on u.AddressId equals a.AddressId
+                          where u.isDeleted == false
+                          orderby u.UserName
+                          select new ClientDto
+                          {
+                              UserId = c.UserId,
+                              ClientId = c.ClientId,
+                              AddressId = u.AddressId,
+                              CellNumber = u.CellNumber,
+                              Email = u.Email,
+                              IdNumber = u.IdNumber,
+                              Initials = u.Initials,
+                              Password = u.Password,
+                              Surname = u.Surname,
+                              UserName = u.UserName,
+                              UserTypeId = u.UserTypeId,
+                              CityCode = a.CityCode,
+                              CityName = a.CityName,
+                              StreetName = a.StreetName,
+                              StreetNumber = a.StreetNumber
+                          }).ToListAsync();
+        }
 
-       
+
+
     }
 }
