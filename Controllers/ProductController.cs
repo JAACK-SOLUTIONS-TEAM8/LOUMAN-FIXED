316f417b4533ee3d00d5f0b6f0f926fd0c72a6fb
JayyyHarden <u19107430@tuks.co.za>
2021-08-16 13:46:24 +0200

    Added Product Controller

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,46 @@
+﻿using Louman.Models.DTOs.Product;
+using Louman.Repositories.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _productReposiroty;
+
+        public ProductController(IProductRepository productReposiroty)
+        {
+            _productReposiroty = productReposiroty;
+        }
+
+        
+
+      
+        [HttpPost("Add")]
+        public async Task<IActionResult> AddNewProduct(ProductDto product)
+        {
+            var newProduct =await  _productReposiroty.AddProduct(product);
+            if (product != null)
+                return Ok(new { Product = newProduct, StatusCode = StatusCodes.Status200OK });
+            return NotFound(new { Product = newProduct, StatusCode = StatusCodes.Status404NotFound });
+
+        }
+
+       
+
+
+
+
+        
+
+      
+
+    }
+}
