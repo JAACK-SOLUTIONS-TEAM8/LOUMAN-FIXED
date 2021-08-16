44305f53288617621c3090d19444545bff3f6931
Ammaar1803 <u18080945@tuks.co.za>
2021-08-16 14:04:20 +0200

    Added add client abstraction

---
+++ Repositories/Abstraction/IClientRepository.cs
@@ -0,0 +1,15 @@
+﻿using Louman.Models.DTOs.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Repositories.Abstraction
+{
+    public interface IClientRepository
+    {
+        Task<ClientDto> Add(ClientDto client);
+        
+
+    }
+}
