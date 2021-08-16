cb4c49e6f67d7348aac6bc4464bab4b87499adfc
Ammaar1803 <u18080945@tuks.co.za>
2021-08-16 23:15:36 +0200

    get all method [Client] controller

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,34 @@
+﻿using Louman.Models.DTOs.Client;
+using Louman.Repositories.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly IClientRepository _clientRepository;
+
+        public ClientController(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        [HttpGet("All")]
+        public async Task<IActionResult> GetAll()
+        {
+            var clients = await _clientRepository.GetAllAsync();
+            if (clients != null)
+                return Ok(new {clients=clients,statusCode=StatusCodes.Status200OK });
+            return Ok(new { clients = clients, statusCode = StatusCodes.Status404NotFound });
+        }
+
+       
+    }
+}
