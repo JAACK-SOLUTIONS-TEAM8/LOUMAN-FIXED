a55264db725e60d8dd353082535c129d88a2da2f
Ammaar1803 <u18080945@tuks.co.za>
2021-08-17 21:51:07 +0200

    add request [user] controller

---
+++ Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Louman.Models.DTOs.User;
+using Louman.Repositories.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> AddNewProductType(UserTypeDto productType)
+        {
+            var newUserType = await _userRepository.AddUserType(productType);
+            if (newUserType != null)
+                return Ok(new { UserType = newUserType, StatusCode = StatusCodes.Status200OK });
+            return NotFound(new { UserType = newUserType, StatusCode = StatusCodes.Status404NotFound });
+
+        }
+
+     
+
+    }
+}
