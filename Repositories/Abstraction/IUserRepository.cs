20192886225b4998fc31056ebf281816d3b99b5f
Ammaar1803 <u18080945@tuks.co.za>
2021-08-17 14:28:09 +0200

    Added Add user type[User] abstraction

---
+++ Repositories/Abstraction/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Louman.Models.DTOs.Admin;
+using Louman.Models.DTOs.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Repositories.Abstraction
+{
+    public interface IUserRepository
+    {
+        Task<UserTypeDto> AddUserType(UserTypeDto userType);
+       
+
+    }
+}
