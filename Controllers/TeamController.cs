b837a6d1e1686b2d62b5d5965c676694ae723080
kayyborra <u19020393@tuks.co.za>
2021-08-17 18:28:49 +0200

    added team controller and add team method

---
+++ Controllers/TeamController.cs
@@ -0,0 +1,36 @@
+﻿using Louman.Models.DTOs.Team;
+using Louman.Repositories.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamController : ControllerBase
+    {
+        private readonly ITeamRepository _teamRepository;
+
+        public TeamController(ITeamRepository teamRepository)
+        {
+           _teamRepository = teamRepository;
+        }
+
+
+        [HttpPost("Add")]
+        public async Task<IActionResult> AddNewTeam(TeamDto team)
+        {
+            var newTeam = await _teamRepository.AddAsync(team);
+            if (newTeam != null)
+                return Ok(new { Team = newTeam, StatusCode = StatusCodes.Status200OK });
+            return NotFound(new { Team = newTeam, StatusCode = StatusCodes.Status404NotFound });
+
+        }
+
+
+    }
+}
