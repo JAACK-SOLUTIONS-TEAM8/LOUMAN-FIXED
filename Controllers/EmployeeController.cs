47be3b700d09eca867433b49577c4ba0a615168a
kayyborra <u19020393@tuks.co.za>
2021-08-17 10:08:19 +0200

    added employee controller & add method

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,36 @@
+﻿using Louman.Models.DTOs.Employee;
+using Louman.Repositories.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+
+        [HttpPost("Upsert")]
+        public async Task<IActionResult> Add([FromBody] EmployeeDto employee)
+        {
+            var emp = await _employeeRepository.Add(employee);
+            if (emp != null)
+                return Ok(new { Employee = emp, statusCode = StatusCodes.Status200OK });
+            return Ok(new { Employee = emp, statusCode = StatusCodes.Status400BadRequest });
+        }
+
+        
+
+    }
+}
