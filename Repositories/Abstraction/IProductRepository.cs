a365c474d0872c700c9e289a5b89083b66048369
ArisjaB <u19002328@tuks.co.za>
2021-08-17 20:45:07 +0200

    Added add product type abstraction

--- Repositories/Abstraction/IProductRepository.cs
+++ Repositories/Abstraction/IProductRepository.cs
@@ -8,8 +8,8 @@ namespace Louman.Repositories.Abstraction
 {
     public interface IProductRepository
     {
-        
 
+        Task<ProductTypeDto> AddProductType(ProductTypeDto productType);
         Task<ProductDto> AddProduct(ProductDto product);
         Task<ProductSizeDto> AddProductSize(ProductSizeDto productSize);
         Task<bool> DeleteProductSize(int productSizeId);
