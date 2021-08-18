b5cc866d2bfb97b6a9a596d96743389729c05183
JayyyHarden <u19107430@tuks.co.za>
2021-08-18 21:10:02 +0200

    Added Get Single response controller method

--- Controllers/EnquiryController.cs
+++ Controllers/EnquiryController.cs
@@ -70,5 +70,15 @@ namespace Louman.Controllers
             return Ok(new { EnquiryResponsesStatus = enquiryResponsesStatus, StatusCode = StatusCodes.Status404NotFound });
 
         }
+
+        [HttpGet("EnquiryResponseStatus/{id}")]
+        public async Task<IActionResult> GetEnquiryResponseStatusById([FromRoute] int id)
+        {
+            var enquiryResponseStatus = await _enquiryRepository.GetEnquiryResponseById(id);
+            if (enquiryResponseStatus != null)
+                return Ok(new { EnquiryResponseStatus = enquiryResponseStatus, StatusCode = StatusCodes.Status200OK });
+            return Ok(new { EnquiryResponseStatus = enquiryResponseStatus, StatusCode = StatusCodes.Status404NotFound });
+
+        }
     }
 }
