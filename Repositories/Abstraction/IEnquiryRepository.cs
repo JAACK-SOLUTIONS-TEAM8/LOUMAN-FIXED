460534a0ea79f39ca9d9ee38c5256c017d59f5b6
kayyborra <u19020393@tuks.co.za>
2021-08-18 15:58:12 +0200

    added enq abstraction and add enq type

---
+++ Repositories/Abstraction/IEnquiryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Louman.Models.DTOs.Enquiry;
+using Louman.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Repositories.Abstraction
+{
+    public interface IEnquiryRepository
+    {
+        Task<EnquiryTypeDto> AddEnquiryType(EnquiryTypeDto enquiryType);
+       
+    }
+    
+}
