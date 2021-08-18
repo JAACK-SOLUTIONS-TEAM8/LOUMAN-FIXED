c72990945958658dceead10c0c267b30a0ab97e1
kayyborra <u19020393@tuks.co.za>
2021-08-18 17:12:39 +0200

    added enq controlller and add enq type method

---
+++ Controllers/EnquiryController.cs
@@ -0,0 +1,34 @@
+﻿using Louman.Models.DTOs.Enquiry;
+using Louman.Repositories.Abstraction;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Louman.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EnquiryController : ControllerBase
+    {
+        private readonly IEnquiryRepository _enquiryRepository;
+
+        public EnquiryController(IEnquiryRepository enquiryRepository)
+        {
+            _enquiryRepository = enquiryRepository;
+        }
+
+        [HttpPost("EnquiryType/Add")]
+        public async Task<IActionResult> AddEnquiryType([FromBody] EnquiryTypeDto enquiryType)
+        {
+            var enquiry = await _enquiryRepository.AddEnquiryType(enquiryType);
+            if (enquiry != null)
+                return Ok(new { EnquiryType = enquiry, StatusCode = StatusCodes.Status200OK });
+            return Ok(new { EnquiryType = enquiry, StatusCode = StatusCodes.Status400BadRequest });
+
+        }
+
+    }
+}
