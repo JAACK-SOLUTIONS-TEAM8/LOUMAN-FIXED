0fa6bd5627280527b20dc080dbc7eec7bedc2a25
ArisjaB <u19002328@tuks.co.za>
2021-08-19 11:18:04 +0200

    added get enquiry with response abstraction

--- Repositories/Abstraction/IEnquiryRepository.cs
+++ Repositories/Abstraction/IEnquiryRepository.cs
@@ -27,6 +27,7 @@ namespace Louman.Repositories.Abstraction
         Task<bool> DeleteEnquiry(int enquiryId);
         Task<List<GetEnquiryDto>> GetAllAdminEnquiryById(int adminUserId);
         Task<List<GetEnquiryDto>> GetAllAdminEnquiryByEnquiryTypeId(int adminUserId, int enquiryTypeId);
+        Task<EnquiryWithResponseDto> GetEnquiryWithResponse(int enquiryId);
 
 
     }
