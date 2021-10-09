1ad2c02e32bc6792160e0d688b76966f80f3c000
JayyyHarden <u19107430@tuks.co.za>
2021-10-09 14:03:29 +0200

    Updated email message

--- Services/MailingService.cs
+++ Services/MailingService.cs
@@ -1,5 +1,7 @@
 ﻿using Louman.Models.DTOs.Client;
 using Louman.Models.DTOs.Email;
+using Louman.Models.DTOs.User;
+using Louman.Models.Entities;
 using MailKit.Net.Smtp;
 using MailKit.Security;
 using Microsoft.AspNetCore.Hosting;
@@ -23,14 +25,14 @@ namespace Louman.Services
         private readonly IConfiguration _configuration;
         private readonly Models.DTOs.Email.MailSettings _mailSettings;
         private readonly IWebHostEnvironment _env;
-        public MailingService(IConfiguration configuration, IOptions<Models.DTOs.Email.MailSettings> mailSettings,IWebHostEnvironment env)
+        public MailingService(IConfiguration configuration, IOptions<Models.DTOs.Email.MailSettings> mailSettings, IWebHostEnvironment env)
         {
             _configuration = configuration;
             _mailSettings = mailSettings.Value;
             _env = env;
 
         }
-        
+
 
         public async Task SendEmailAsync(ClientDto client)
         {
@@ -41,15 +43,16 @@ namespace Louman.Services
             var builder = new BodyBuilder();
 
 
-            var filePath = Path.Combine(_env.ContentRootPath,"wwwroot\\images", "louman.jpeg");
+            var filePath = Path.Combine(_env.ContentRootPath, "wwwroot\\images", "louman.jpeg");
 
-           
+
             var image = builder.LinkedResources.Add(filePath);
             image.ContentId = MimeUtils.GenerateMessageId();
-            builder.HtmlBody = @$"Dear {client.Initials}, <br><br>
+            builder.HtmlBody = @$"Dear {client.Initials + client.Surname}, <br><br>
                                 Welcome to the Louman System! We are so happy you joined us and we hope that our<br>
-                                application will be of great service to you.If you have anyt question / queuries regarding products or<br>
-                                any other thing, don't be shy to message us!<br><br>
+                                application will be of great service to you.If you have any questions or queuries regarding products or<br>
+                                any other thing, don't be shy to message us! You can make these queries directly on this email or by booking a meeting via the system
+or making use of the enquiry section that can also be found on our system!<br><br>
                                 Kind regards,<br> 
                                 The Louman Team.<br><img height=""400px"" width=""400px"" src=""cid:{image.ContentId}"">";
             email.Body = builder.ToMessageBody();
@@ -60,5 +63,21 @@ namespace Louman.Services
             smtp.Disconnect(true);
         }
 
+        public async Task SendEmailVerificationCode(UserDto user, string verificationCode)
+        {
+            var email = new MimeMessage();
+            email.Sender = MailboxAddress.Parse(_mailSettings.Mail);
+            email.To.Add(MailboxAddress.Parse(user.Email));
+            email.Subject = "Verification Code";
+            var builder = new BodyBuilder();
+
+            builder.HtmlBody = @$"<p>Your verification code: <span><b>{verificationCode}</b></span></p>";
+            email.Body = builder.ToMessageBody();
+            using var smtp = new SmtpClient();
+            smtp.Connect(_mailSettings.Host, _mailSettings.Port, SecureSocketOptions.StartTls);
+            smtp.Authenticate(_mailSettings.Mail, _mailSettings.Password);
+            await smtp.SendAsync(email);
+            smtp.Disconnect(true);
+        }
     }
 }
